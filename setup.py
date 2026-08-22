c6aedd30b3a57e8b696cebde51c547fae2d5566e
Jef-NL <jefbaars@gmail.com>
2026-08-22 10:28:40 +0200

    Bup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="trmnl_webhook_push",
-    version="1.0.0",
+    version="1.1.1",
     packages=["trmnl_webhook_push"],
     install_requires=["requests"],
 ) 
